e506c48ba2ffb5601b06f9d1d8cdcddb68f9919b
caizhengjun <zhengjun_cai@loongjoy.com>
2019-02-13 14:24:21 +0800

    add admin's bicycle manage model

---
+++ application/index/model/Bicycle.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\index\model;
+
+use think\Model;
+use traits\model\SoftDelete;
+
+class Bicycle extends Model
+{
+    use SoftDelete;
+    /**
+     * @var string
+     */
+    protected $table = "bicycles";
+
+    /**
+     * @var string
+     */
+    protected $updateTime = 'updated';
+
+    /**
+     * @var string
+     */
+    protected $createTime = 'created';
+
+    /**
+     * @var string
+     */
+    protected static $deleteTime = 'deleted';
+
+    /**
+     * @var bool
+     */
+    protected $autoWriteTimestamp = true;
+
+    /**
+     * @var string
+     */
+    protected $pk = 'id';
+
+
+    protected $field = [
+        'created',
+        'updated',
+        'deleted'
+    ];
+}
