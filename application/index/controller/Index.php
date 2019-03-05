a4f7bff509055646a1803855b8e65f962f5cf421
caizhengjun <zhengjun_cai@loongjoy.com>
2019-03-05 12:55:48 +0800

    [fix] 1. 首页加上chart图

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,6 +2,7 @@
 
 namespace app\index\controller;
 
+use app\common\Utils;
 use think\Request;
 use app\index\model\User as UserModel;
 use app\index\model\Client\Operation as ClientOperationModel;
@@ -26,69 +27,44 @@ class Index extends Base
      * @return mixed
      * @throws \think\Exception
      */
-    public function index()
+    public function index(Request $request)
     {
-        $userCount = UserModel::where('deleted', '=', 0)->count();   //总用户
+        //订单
+        $startTime = strtotime($request->param('start', date('Y-m-1 00:00:00')));   //开始时间
+        $endTime = strtotime($request->param('end', date('Y-m-t 23:59:59')));   //结束时间
+        $orders = OrderModel::where('deleted', '=', 0)
+            ->where('end', '>=', $startTime)
+            ->where('end', '<=', $endTime)
+            ->select();
+        $ordersGroupByDay = [];
+        foreach ($orders as $order) {
+            $orderTime = date('Y-m-d', strtotime($order->end));
+            $ordersGroupByDay[$orderTime]['count'] = ($ordersGroupByDay[$orderTime]['count'] ?? 0) + 1;
+            $ordersGroupByDay[$orderTime]['sumPrice'] = ($ordersGroupByDay[$orderTime]['sumPrice'] ?? 0) + $order->price;
+        }
+        if ($request->isAjax()) {
+            return Utils::ajaxReturn($ordersGroupByDay);
+        } else {
+            $userCount = UserModel::where('deleted', '=', 0)->count();   //总用户
+            $dayAddUser = ClientModel::whereTime('created', 'today')->where('deleted', '=', 0)->count();   //当前周新增用户
 
-        $monthActiveUser = ClientOperationModel::whereTime('time', 'month')->count();   //当前月活跃用户
-
-        $weekActiveUser = ClientOperationModel::whereTime('time', 'week')->count();   //当前周活跃用户
-
-        $dayActiveUser = ClientOperationModel::whereTime('time', 'today')->count();   //当前周活跃用户
-
-        $monthAddUser = ClientModel::whereTime('created', 'month')->where('deleted', '=', 0)->count();   //当前月新增用户
-
-        $weekAddUser = ClientModel::whereTime('created', 'week')->where('deleted', '=', 0)->count();   //当前周新增用户
-
-        $dayAddUser = ClientModel::whereTime('created', 'today')->where('deleted', '=', 0)->count();   //当前周新增用户
-
-        //当月骑行单数
-        $monthCyclingOrderNumber = OrderModel::whereTime('created', 'month')->where('deleted', '=', 0)->count();
-        //当周骑行单数
-        $weekCyclingOrderNumber = OrderModel::whereTime('created', 'week')->where('deleted', '=', 0)->count();
-        //当日骑行单数
-        $dayCyclingOrderNumber = OrderModel::whereTime('created', 'today')->where('deleted', '=', 0)->count();
-
-        //当月已支付单数
-        $monthPayOrderNumber = OrderModel::whereTime('created', 'month')->where('status', '=', 3)->where('deleted', '=', 0)->count();
-        //当周已支付单数
-        $weekPayOrderNumber = OrderModel::whereTime('created', 'week')->where('status', '=', 3)->where('deleted', '=', 0)->count();
-        //当天已支付单数
-        $dayPayOrderNumber = OrderModel::whereTime('created', 'today')->where('status', '=', 3)->where('deleted', '=', 0)->count();
-
-        //当月未支付单数
-        $monthNoPayOrderNumber = OrderModel::whereTime('created', 'month')->where('status', '=', 4)->where('deleted', '=', 0)->count();
-        //当周未支付单数
-        $weekNoPayOrderNumber = OrderModel::whereTime('created', 'week')->where('status', '=', 4)->where('deleted', '=', 0)->count();
-        //当天未支付单数
-        $dayNoPayOrderNumber = OrderModel::whereTime('created', 'today')->where('status', '=', 4)->where('deleted', '=', 0)->count();
+            //当月骑行单数
+            $monthCyclingOrderNumber = OrderModel::whereTime('created', 'month')->where('deleted', '=', 0)->count();
+            //当月应收金额
+            $monthReceivableMoney = OrderModel::whereTime('end', 'month')->where(function ($query) {
+                $query->where('status', '=', 3)
+                    ->whereOr('status', '=', 4);
+            })->where('deleted', '=', 0)->sum('price');
+            //当月实收金额
+            $monthPaidAmount = OrderModel::whereTime('end', 'month')->where('status', '=', 3)->where('deleted', '=', 0)->sum('price');
+            //当月待收金额
+            $monthAmountToBeReceived = OrderModel::whereTime('end', 'month')->where('status', '=', 4)->where('deleted', '=', 0)->sum('price');
 
 
-        //当月应收金额
-        $monthReceivableMoney = OrderModel::whereTime('created', 'month')->where('status', '=', 3)->whereOr('status', '=', 4)->where('deleted', '=', 0)->sum('price');
-        //当周应收金额
-        $weekReceivableMoney = OrderModel::whereTime('created', 'week')->where('status', '=', 3)->whereOr('status', '=', 4)->where('deleted', '=', 0)->sum('price');
-        //当天应收金额
-        $dayReceivableMoney = OrderModel::whereTime('created', 'today')->where('status', '=', 3)->whereOr('status', '=', 4)->where('deleted', '=', 0)->sum('price');
-
-        //当月实收金额
-        $monthPaidAmount = OrderModel::whereTime('created', 'month')->where('status', '=', 3)->where('deleted', '=', 0)->sum('price');
-        //当周实收金额
-        $weekPaidAmount = OrderModel::whereTime('created', 'week')->where('status', '=', 3)->where('deleted', '=', 0)->sum('price');
-        //当天实收金额
-        $dayPaidAmount = OrderModel::whereTime('created', 'today')->where('status', '=', 3)->where('deleted', '=', 0)->sum('price');
-
-        //当月待收金额
-        $monthAmountToBeReceived = OrderModel::whereTime('created', 'month')->where('status', '=', 4)->where('deleted', '=', 0)->sum('price');
-        //当周待收金额
-        $weekAmountToBeReceived = OrderModel::whereTime('created', 'week')->where('status', '=', 4)->where('deleted', '=', 0)->sum('price');
-        //当天待收金额
-        $dayAmountToBeReceived = OrderModel::whereTime('created', 'today')->where('status', '=', 4)->where('deleted', '=', 0)->sum('price');
-
-
-
-        $this->assign(compact('userCount', 'monthActiveUser', 'weekActiveUser', 'dayActiveUser', 'weekAddUser', 'monthAddUser', 'dayAddUser', 'monthAmountToBeReceived', 'monthCyclingOrderNumber', 'monthNoPayOrderNumber', 'monthPaidAmount', 'monthPayOrderNumber', 'monthReceivableMoney', 'weekAmountToBeReceived', 'weekCyclingOrderNumber', 'weekNoPayOrderNumber', 'weekPaidAmount', 'weekPayOrderNumber', 'weekReceivableMoney', 'dayAmountToBeReceived', 'dayCyclingOrderNumber', 'dayNoPayOrderNumber', 'dayPaidAmount', 'dayPayOrderNumber', 'dayReceivableMoney'));
-        return $this->fetch();
+            $this->assign(['ordersGroupByDay' => json_encode($ordersGroupByDay)]);
+            $this->assign(compact('userCount', 'dayAddUser', 'monthAmountToBeReceived', 'monthCyclingOrderNumber', 'monthPaidAmount', 'monthReceivableMoney'));
+            return $this->fetch();
+        }
     }
 
     /**
